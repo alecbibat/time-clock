692a4ded076f2f2508908dff2303dee18c284c08
alecbibat <alecbibat@gmail.com>
2025-05-08 16:38:16 -0600

    Update timezones.js

--- timezones.js
+++ timezones.js
@@ -3,6 +3,7 @@ const timeZones = [
   { name: "America/New_York", label: "Eastern Time (New York)", color: "#ffd6a5" },
   { name: "America/Chicago", label: "Central Time (Chicago)", color: "#fdffb6" },
   { name: "America/Denver", label: "Mountain Time (Denver)", color: "#caffbf" },
+  { name: "America/Denver", label: "Rocky Mountain National Park Time", color: "#caffbf" },
   { name: "America/Los_Angeles", label: "Pacific Time (LA)", color: "#9bf6ff" },
   { name: "America/Anchorage", label: "Alaska Time", color: "#bdb2ff" },
   { name: "Pacific/Honolulu", label: "Hawaii Time", color: "#ffc6ff" },
