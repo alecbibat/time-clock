8784dc15a5b4bd3601e33eef597edfc56e19f02c
alecbibat <alecbibat@gmail.com>
2025-05-12 15:11:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,28 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 fetch('timezones_wVVG8.geojson')
   .then(response => response.json())
   .then(data => {
-    L.geoJSON(data, {
+    const wrappedFeatures = [];
+
+    data.features.forEach(feature => {
+      // Original feature
+      wrappedFeatures.push(feature);
+
+      // Clone with shifted longitudes: -360 and +360
+      const shiftedLeft = JSON.parse(JSON.stringify(feature));
+      const shiftedRight = JSON.parse(JSON.stringify(feature));
+
+      shiftLongitude(shiftedLeft, -360);
+      shiftLongitude(shiftedRight, 360);
+
+      wrappedFeatures.push(shiftedLeft, shiftedRight);
+    });
+
+    const wrappedGeoJSON = {
+      type: "FeatureCollection",
+      features: wrappedFeatures
+    };
+
+    L.geoJSON(wrappedGeoJSON, {
       style: {
         color: '#555',
         weight: 1,
@@ -19,6 +40,22 @@ fetch('timezones_wVVG8.geojson')
       }
     }).addTo(map);
   });
+
+function shiftLongitude(feature, offset) {
+  function shiftCoords(coords) {
+    return coords.map(coord => {
+      if (Array.isArray(coord[0])) {
+        return shiftCoords(coord);
+      } else {
+        return [coord[0] + offset, coord[1]];
+      }
+    });
+  }
+
+  if (feature.geometry && feature.geometry.coordinates) {
+    feature.geometry.coordinates = shiftCoords(feature.geometry.coordinates);
+  }
+}
 
 function updateClock(elementId, timeZone) {
   const el = document.getElementById(elementId);
